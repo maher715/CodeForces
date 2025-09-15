f5de3a362ac4a379670248f777cf35c4e1635b30
maher715 <e243017@ugrad.iiuc.ac.bd>
2025-09-15 22:13:10 +0600

    Update K.cpp

--- Sheet 1/K.cpp
+++ Sheet 1/K.cpp
@@ -1,1 +1,23 @@
-
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main (){
+long long A,B,C;
+cin>>A>>B>>C;
+ 
+if (A<=B && A<=C){
+    cout<<A<<" ";
+} else if(B<=A && B<=C) {
+    cout<<B<<" ";
+} else if (C<=B && C<=A) {
+    cout<<C<<" ";
+}
+ 
+if (A>=B && A>=C){
+    cout<<A<<endl;
+} else if(B>=A && B>=C) {
+    cout<<B<<endl;
+} else if (C>=A && C>=B) {
+    cout<<C<<endl;
+}
+}
